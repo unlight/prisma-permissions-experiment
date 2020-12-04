bf9fd09c4fedec6fe6ff78d6692659e3e19d759e
roman <s91630277@gmail.com>
2020-12-04 23:17:32 +0400

    rework

---
+++ src/feed.ts
@@ -0,0 +1,86 @@
+import { PrismaClient, Prisma } from '@prisma/client';
+
+const prisma = new PrismaClient({
+	log: ['query'],
+});
+
+async function main() {
+	// @ts-ignore
+	prisma.$on('query', (event: any) => {
+		console.log('prisma:query:params', event.params);
+	});
+
+	const aliceUser = await prisma.user.findUnique({
+		where: { name: 'Alice' },
+	});
+	console.log('aliceUser', aliceUser);
+	console.log('Alice should see `Hello all`, `Post without category`');
+
+	const aliceFeed = await prisma.post.findMany({
+		where: {
+			category: {
+				permission: {
+					OR: [
+						{
+							categoryId: { equals: null },
+						},
+						{
+							role: {
+								users: {
+									some: {
+										userId: aliceUser.userId,
+									},
+								},
+							},
+						},
+					],
+				},
+			},
+		},
+	});
+	console.log('aliceFeed', aliceFeed);
+
+	const bobUser = await prisma.user.findUnique({
+		select: {
+			userId: true,
+			name: true,
+			roles: true,
+		},
+		where: { name: 'Bob' },
+	});
+
+	console.log('bob user', bobUser);
+
+	const bobFeed = await prisma.post.findMany({
+		where: {
+			category: {
+				permission: {
+					OR: [
+						{
+							categoryId: { equals: null },
+						},
+						{
+							role: {
+								users: {
+									some: {
+										userId: bobUser.userId,
+									},
+								},
+							},
+						},
+					],
+				},
+			},
+		},
+	});
+	console.log('bobFeed', bobFeed);
+}
+
+main()
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	})
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
--- src/seed.ts
+++ src/seed.ts
@@ -6,210 +6,104 @@ const prisma = new PrismaClient({
 
 // @ts-ignore
 prisma.$on('query', (event: any) => {
-  if (event.params === '[]') return;
   console.log('prisma:query:params', event.params);
 });
 
 // A `main` function so that we can use async/await
 async function main() {
-  // Create roles
-  const guest = await prisma.role.create({
-    data: {
-      name: 'Guest',
-    },
-  });
-  const applicantRole = await prisma.role.create({
-    data: {
-      name: 'Applicant',
-    },
-  });
-  const watcherRole = await prisma.role.create({
-    data: {
-      name: 'Watcher',
-    },
-  });
   // Create categories
-  const categoryForApplicants = await prisma.category.create({
-    data: {
-      name: 'ForApplicants',
-    },
-  });
-  const categoryForUsers = await prisma.category.create({
+  const category1 = await prisma.category.create({
     data: {
       name: 'ForUsers',
     },
   });
-
-  // Seed the database with users and posts
-  const user1 = await prisma.user.create({
+  const category2 = await prisma.category.create({
     data: {
-      email: 'alice@prisma.io',
-      name: 'Alice',
-      posts: {
-        create: {
-          title: 'Watch the talks from Prisma Day 2019',
-          content: 'https://www.prisma.io/blog/z11sg6ipb3i1/',
-          published: true,
-          category: {
-            connect: {
-              categoryId: categoryForApplicants.categoryId,
-            },
-          },
-        },
-      },
-      roles: {
-        connect: {
-          roleId: applicantRole.roleId,
-        },
-      },
-    },
-    include: {
-      posts: true,
+      name: 'ForAdmins',
     },
   });
 
-  const user2 = await prisma.user.create({
+  // Create users with roles and posts
+  await prisma.user.create({
     data: {
-      email: 'bob@prisma.io',
-      name: 'Bob',
-      posts: {
+      name: 'Alice',
+      roles: {
         create: [
           {
-            title: 'Subscribe to GraphQL Weekly for community news',
-            content: 'https://graphqlweekly.com/',
-            published: true,
-            category: {
-              connect: {
-                categoryId: categoryForUsers.categoryId,
-              },
-            },
-          },
-          {
-            title: 'Follow Prisma on Twitter',
-            content: 'https://twitter.com/prisma/',
-            published: false,
-            category: {
-              connect: {
-                categoryId: categoryForUsers.categoryId,
-              },
-            },
+            // roleId: 1,
+            name: 'User',
           },
         ],
       },
+    },
+  });
+  await prisma.user.create({
+    data: {
+      name: 'Bob',
       roles: {
         connect: {
-          roleId: watcherRole.roleId,
+          roleId: 1, // User
         },
+        create: [
+          {
+            // roleId: 2,
+            name: 'Admin',
+          },
+        ],
+      },
+      posts: {
+        create: [
+          {
+            title: 'Hello all',
+            category: {
+              connect: {
+                categoryId: category1.categoryId,
+              },
+            },
+          },
+          {
+            title: 'For admins',
+            category: {
+              connect: {
+                categoryId: category2.categoryId,
+              },
+            },
+          },
+          {
+            title: 'Post without category',
+          },
+        ],
       },
     },
-    include: {
-      posts: true,
-    },
   });
-  console.log(
-    `Created users: ${user1.name} (${user1.posts.length} post) and ${user2.name} (${user2.posts.length} posts) `,
-  );
 
-  // Retrieve all published posts
-  const allPosts = await prisma.post.findMany({
-    where: { published: true },
-  });
-  console.log(`Retrieved all published posts: `, allPosts);
-
-  // Create a new post (written by an already existing user with email alice@prisma.io)
-  const newPost = await prisma.post.create({
+  // Define permissions
+  // Users (roleId: 1) can view posts only in category1
+  await prisma.permission.create({
     data: {
-      title: 'Join the Prisma Slack community',
-      content: 'http://slack.prisma.io',
-      published: false,
-      author: {
+      viewPosts: true,
+      role: {
         connect: {
-          email: 'alice@prisma.io',
+          roleId: 1,
         },
       },
       category: {
         connect: {
-          categoryId: categoryForUsers.categoryId,
+          categoryId: category1.categoryId,
         },
       },
     },
   });
-  console.log(`Created a new post: `, newPost);
-
-  // Publish the new post
-  const updatedPost = await prisma.post.update({
-    where: {
-      id: newPost.id,
-    },
-    data: {
-      published: true,
-    },
-  });
-  console.log(`Published the newly created post: `, updatedPost);
-
-  // Retrieve all posts by user with email alice@prisma.io
-  const postsByUser = await prisma.user
-    .findUnique({
-      where: {
-        email: 'alice@prisma.io',
-      },
-    })
-    .posts();
-  console.log(`Retrieved all posts from a specific user: `, postsByUser);
-
-  // Define permissions
-  await prisma.permission.create({
-    data: {
-      registration: true,
-      viewPosts: false,
-      role: {
-        connect: {
-          roleId: guest.roleId,
-        },
-      },
-    },
-  });
-
-  // Applicant can view only applicant category
-
-  await prisma.permission.create({
-    data: {
-      viewPosts: true,
-      junctionTable: 'Category',
-      junctionColumn: 'categoryId',
-      junctionId: categoryForApplicants.categoryId,
-      role: {
-        connect: {
-          roleId: applicantRole.roleId,
-        },
-      },
-    },
-  });
-
-  await prisma.permission.create({
-    data: {
-      viewPosts: false,
-      junctionTable: 'Category',
-      junctionColumn: 'categoryId',
-      junctionId: categoryForUsers.categoryId,
-      role: {
-        connect: {
-          roleId: applicantRole.roleId,
-        },
-      },
-    },
-  });
-
-  // Watcher can view any category
-
+  // Admins (roleId: 2) can view posts in any categories (category = null)
   await prisma.permission.create({
     data: {
       viewPosts: true,
       role: {
         connect: {
-          roleId: watcherRole.roleId,
+          roleId: 2,
         },
       },
+      // category: null
     },
   });
 }
